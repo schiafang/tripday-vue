0ec4f8835cbdc28eb368830653a0bc8e90dfb0af
carey <schiafang@gmail.com>
2020-09-17 16:44:59 +0800

    fix: bug

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -17,65 +17,65 @@
           </div>
         </template>
 
-        <div
-          class="order-list"
-          v-else
-          v-for="(order, index) in orderList"
-          :key="index"
-        >
-          <input
-            type="checkbox"
-            :id="index + 'orderContent'"
-            class="order-input "
-            @click="openList"
-          />
-          <label :for="index + 'orderContent'" class="top">
-            <i class="icon-arrow-up"></i>
-            <h4 class="mt-4">
-              {{ order.bookingDetail.plan.planOption[0].title }}
-            </h4>
-            <div class="item">
-              <i class="icon-people"></i>
-              <span
-                class="inline"
-                v-for="(type, index) in order.bookingDetail.type"
-                :key="index"
-              >
-                {{ type.name }} x {{ type.quantity }}
-              </span>
-            </div>
-            <div class="order-list-number">
-              <h6>訂單編號 #XDEW323087</h6>
-            </div>
-          </label>
+        <template v-else>
+          <div
+            class="order-list"
+            v-for="(order, index) in orderList"
+            :key="index"
+          >
+            <input
+              type="checkbox"
+              :id="index + 'orderContent'"
+              class="order-input "
+            />
+            <label :for="index + 'orderContent'" class="top">
+              <i class="icon-arrow-up"></i>
+              <h4 class="mt-4">
+                {{ order.bookingDetail.plan.planOption[0].title }}
+              </h4>
+              <div class="item">
+                <i class="icon-people"></i>
+                <span
+                  class="inline"
+                  v-for="(type, index) in order.bookingDetail.type"
+                  :key="index"
+                >
+                  {{ type.name }} x {{ type.quantity }}
+                </span>
+              </div>
+              <div class="order-list-number">
+                <h6>訂單編號 #XDEW323087</h6>
+              </div>
+            </label>
 
-          <div class="content">
-            <div class="item">
-              <i class="icon-calendar"></i>
-              <span> 日期:</span>{{ order.bookingDetail.date }}
-            </div>
-            <div class="item">
-              <i class="icon-user"></i>
-              <span> 訂購人:</span>{{ order.fullName }}
-            </div>
-            <div class="item">
-              <i class="icon-user-following"></i>
-              <span> 主要聯絡人:</span>{{ order.mainPassenger }}
-            </div>
-            <div class="item">
-              <i class="icon-phone"></i>
-              <span> 聯絡電話:</span>{{ order.contactNumber }}
-            </div>
-            <div class="item">
-              <i class="icon-note"></i>
-              <span> 備註:</span> {{ order.reminder || '無' }}
-            </div>
-            <div class="item">
-              <i class="icon-credit-card"></i>
-              <span> 訂單金額:</span> {{ order.billPrice }}
+            <div class="content">
+              <div class="item">
+                <i class="icon-calendar"></i>
+                <span> 日期:</span>{{ order.bookingDetail.date }}
+              </div>
+              <div class="item">
+                <i class="icon-user"></i>
+                <span> 訂購人:</span>{{ order.fullName }}
+              </div>
+              <div class="item">
+                <i class="icon-user-following"></i>
+                <span> 主要聯絡人:</span>{{ order.mainPassenger }}
+              </div>
+              <div class="item">
+                <i class="icon-phone"></i>
+                <span> 聯絡電話:</span>{{ order.contactNumber }}
+              </div>
+              <div class="item">
+                <i class="icon-note"></i>
+                <span> 備註:</span> {{ order.reminder || '無' }}
+              </div>
+              <div class="item">
+                <i class="icon-credit-card"></i>
+                <span> 訂單金額:</span> {{ order.billPrice }}
+              </div>
             </div>
           </div>
-        </div>
+        </template>
       </div>
     </div>
   </div>
