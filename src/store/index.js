2e105e907969074dec7f7f7b677b401fd05fd408
Carey <schiafang@gmail.com>
2021-06-19 20:40:56 +0800

    fix: currency api

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import axios from 'axios'
+import axios from 'axios'
 import authorizationAPI from '../apis/authorization'
-import createPersistedState from "vuex-persistedstate";
-import SecureLS from "secure-ls";
-var ls = new SecureLS({ encodingType: "aes", isCompression: false });
+import createPersistedState from 'vuex-persistedstate'
+import SecureLS from 'secure-ls'
+
+var ls = new SecureLS({ encodingType: 'aes', isCompression: false })
 
 Vue.use(Vuex)
 
@@ -17,7 +18,7 @@ export default new Vuex.Store({
       avatar: '',
       email: '',
       tel: '',
-      isAdmin: false
+      isAdmin: false,
     },
     favoriteProducts: [],
     isAuthenticated: false,
@@ -27,7 +28,7 @@ export default new Vuex.Store({
     exchangeRate: 1,
     currentCurrency: 'TWD',
     attrs: false,
-    on: false
+    on: false,
   },
   mutations: {
     setCurrentUser(state, user) {
@@ -46,7 +47,7 @@ export default new Vuex.Store({
         avatar: '',
         email: '',
         tel: '',
-        isAdmin: false
+        isAdmin: false,
       }
       state.isAuthenticated = false
       state.token = ''
@@ -58,32 +59,45 @@ export default new Vuex.Store({
     },
     removeFavorite(state, id) {
       if (state.favoriteProducts.includes(id)) {
-        state.favoriteProducts = state.favoriteProducts.filter(i => i !== id)
+        state.favoriteProducts = state.favoriteProducts.filter((i) => i !== id)
       }
-    }
+    },
   },
   actions: {
     async fetchCurrentUser({ commit }) {
       try {
         const { data } = await authorizationAPI.getCurrentUser()
-        if (status === 'error') { throw new Error(data) }
+        if (status === 'error') {
+          throw new Error(data)
+        }
         const { id, name, email, isAdmin, realname, avatar, tel } = data
-        commit('setCurrentUser', { id, name, email, isAdmin, realname, avatar, tel })
+        commit('setCurrentUser', {
+          id,
+          name,
+          email,
+          isAdmin,
+          realname,
+          avatar,
+          tel,
+        })
       } catch (error) {
         console.error(error)
         commit('revokeAuthentication')
       }
     },
     async getCurrency(_, currency) {
-      // const res = await axios.get(`https://data.fixer.io/api/latest?access_key=acbd6972196dcf75fac8c856311702c0&symbols=${currency},TWD&format=1`)
+      const res = await axios.get(
+        `http://data.fixer.io/api/latest?access_key=acbd6972196dcf75fac8c856311702c0&symbols=${currency},TWD&format=1`
+      )
 
-      // const TWD = res.data.rates.TWD
+      const TWD = res.data.rates.TWD
       let exchange = 28.992198027261733
 
       if (currency === 'USD') {
-        // if (res) {
-        //   exchange = TWD / res.data.rates.USD
-        // }
+        if (res) {
+          exchange = TWD / res.data.rates.USD
+        }
+
         this.state.exchangeRate = exchange
         this.state.currentCurrency = 'USD'
       }
@@ -92,10 +106,9 @@ export default new Vuex.Store({
         this.state.exchangeRate = 1
         this.state.currentCurrency = 'TWD'
       }
-    }
+    },
   },
-  modules: {
-  },
+  modules: {},
   plugins: [
     createPersistedState({
       key: 'userInfo',
@@ -104,14 +117,14 @@ export default new Vuex.Store({
           user: state.user,
           favoriteProducts: state.favoriteProducts,
           isAuthenticated: state.isAuthenticated,
-          token: state.token
+          token: state.token,
         }
       },
       storage: {
-        getItem: key => ls.get(key),
+        getItem: (key) => ls.get(key),
         setItem: (key, value) => ls.set(key, value),
-        removeItem: key => ls.remove(key)
-      }
-    })
+        removeItem: (key) => ls.remove(key),
+      },
+    }),
   ],
 })
