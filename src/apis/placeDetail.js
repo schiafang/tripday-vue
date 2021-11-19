0b48076e4f2835ac29befe50826a94e1ce9b2079
schiafang <schiafang@gmail.com>
2021-11-19 18:39:28 +0800

    fix: cors

--- src/apis/placeDetail.js
+++ src/apis/placeDetail.js
@@ -1,15 +1,25 @@
 import axios from 'axios'
+
 const corsAnywhere = 'https://cors-anywhere.herokuapp.com/'
+
+const googleInstance = axios.create({
+  baseURL: corsAnywhere + 'https://maps.googleapis.com/maps/api/',
+  headers: { 'x-cors-grida-api-key': process.env.VUE_APP_CORS_KEY },
+})
 
 export default {
   getPlaceID(name) {
-    return axios.get(`${corsAnywhere}https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${process.env.VUE_APP_GOOGLEAPI}&inputtype=textquery&input=${name}`).then(res => {
-      return res
-    })
+    return googleInstance
+      .get(
+        `place/findplacefromtext/json?key=${process.env.VUE_APP_GOOGLEAPI}&inputtype=textquery&input=${name}`
+      )
+      .then((res) => res)
   },
   getPlaceDetail(placeID) {
-    return axios.get(`${corsAnywhere}https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeID}&fields=name,opening_hours,rating,reviews&key=${process.env.VUE_APP_GOOGLEAPI}`).then(res => {
-      return res
-    })
-  }
+    return googleInstance
+      .get(
+        `place/details/json?placeid=${placeID}&fields=name,opening_hours,rating,reviews&key=${process.env.VUE_APP_GOOGLEAPI}`
+      )
+      .then((res) => res)
+  },
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,13 +4,16 @@ const corsAnywhere = 'https://cors.bridged.cc/'
 
 export const axiosInstance = axios.create({
   baseURL: `${corsAnywhere}https://tripday-api.herokuapp.com`,
+  headers: { 'x-cors-grida-api-key': process.env.VUE_APP_CORS_KEY },
 })
 
 export const axiosInstanceJSON = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? `${corsAnywhere}https://tripday-json-server.vercel.app`
-      : 'http://localhost:3000/',
+  // baseURL:
+  //   process.env.NODE_ENV === 'production'
+  //     ? `${corsAnywhere}https://tripday-json-server.vercel.app`
+  //     : 'http://localhost:8000/',
+  baseURL: `${corsAnywhere}https://tripday-json-server.vercel.app`,
+  headers: { 'x-cors-grida-api-key': process.env.VUE_APP_CORS_KEY },
 })
 
 axiosInstance.interceptors.request.use(
